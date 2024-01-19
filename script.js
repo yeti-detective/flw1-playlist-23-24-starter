881caab8491a4cce2ad456fa3c85787d9e7cad9f
chris 🤖 🧙‍♂️ <christopher.martinbrown@flatiron.com>
2024-01-18 19:08:10 -0500

    added some helpful web API functions

--- script.js
+++ script.js
@@ -1,12 +1,83 @@
 // Declare variables below
 
+/**
+ * JSON.stringifies `songList` & saves it to localStorage under the key "songList"
+ * @param songList
+ */
+function saveSongList(songList) {
+  localStorage.setItem('songList', JSON.stringify(songList))
+}
+
+/**
+ * Returns the songList from localStorage, parsed it into an array
+ * @returns {[]}
+ */
+function getSongList() {
+  return JSON.parse(localStorage.getItem('songList'))
+}
+
+/**
+ * returns an object of the song info
+ * @param title {string}
+ * @param artist {string}
+ * @param imageUrl {string}
+ * @param songUrl {string}
+ * @returns {{artist, imageUrl, title, songUrl}}
+ */
+function makeSongObject(title, artist, imageUrl, songUrl) {
+  return {title, artist, imageUrl, songUrl}
+}
+
+const getColumn = columnId => document.getElementById(columnId)
+const getDiv = () => document.createElement('div');
+const getTextDisplay = text => {
+  const div = getDiv();
+  const h3 = document.createElement('h3');
+  h3.innerText = text;
+  return div;
+}
+
+const appendTextDisplayToColumn = (columnId, text) => {
+  const col = getColumn(columnId);
+  col.appendChild(getTextDisplay(text));
+}
+
+function appendImageUrl(imageUrl) {
+  const imagesCol = getColumn('images')
+  const newImage = document.createElement('img')
+  newImage.src = imageUrl;
+  imagesCol.appendChild(newImage);
+}
+
+function appendTitle(title) {
+  appendTextDisplayToColumn('songs', title)
+}
+
+function appendArtist(artistName) {
+  appendTextDisplayToColumn('artists', artistName)
+}
+
+function appendSongUrl(songUrl, title) {
+  const linksCol = getColumn('links');
+  const anchor = document.createElement('a')
+  anchor.href = songUrl
+  anchor.target = "_blank"
+  anchor.innerText = `Listen to ${title}`;
+  anchor.style.fontWeight = 'bold'
+  linksCol.appendChild(anchor)
+}
 
 // Use the body of this function to display your playlist.
 function displaySongInfo() {
+  const songList = getSongList();
 
-
-
+  for (const {imageUrl, title, artist, songUrl} of songList) {
+    appendImageUrl(imageUrl);
+    appendArtist(artist);
+    appendTitle(title);
+    appendSongUrl(songUrl, title)
   }
+}
   
 
 
